e2c3bb79d7674e2affddb03f7b9d28abe85b995d
wiweb123 <william.cremon@db1.com.br>
2026-05-05 16:30:13 -0300

    fix cors for lovable

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "*"
+}));
 
 const PORT = process.env.PORT || 3000;
 
